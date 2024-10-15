29e3350598cda5c17a6252a654f144d6044727d9
Eduardo <dukahenrick06@gmail.com>
2024-10-15 08:48:59 -0300

    coisas feitas

--- Passaro/MainPage.xaml.cs
+++ Passaro/MainPage.xaml.cs
@@ -2,23 +2,108 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
+	const int gravidade = 1;
+	const int tempoEntreFrames = 25;
+	bool estaMorto = false;
+	double larguraJanela = 0;
+	double alturaJanela = 0;
+	int velocidade = 20;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	void AplicaGravidade()
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		imgesquilo.TranslationY += gravidade;
 	}
+	
+
+	void Oi(object s, TappedEventArgs e)
+	{
+		FrameGameOver.IsVisible = false;
+		estaMorto = false;
+		Inicializar();
+		Desenha();
+	}
+
+	void Inicializar()
+	{
+		imgesquilo.TranslationY = 0;
+	}
+
+
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		larguraJanela = w;
+		alturaJanela = h;
+	}
+
+	void GerenciaCanos()
+	{
+		imgcanoceu.TranslationX -= velocidade;
+		imgcanoterra.TranslationX -= velocidade;
+		if (imgcanoterra.TranslationX < -larguraJanela)
+		{
+			imgcanoterra.TranslationX = 0;
+			imgcanoceu.TranslationX = 0;
+		}
+	}
+
+	bool VerificaColisaoTeto()
+	{
+		var minY = -alturaJanela / 2;
+		if (imgesquilo.TranslationY <= minY)
+			return true;
+		else
+			return false;
+	}
+
+	bool VerificaColisaoChao()
+	{
+		var maxY = alturaJanela / 2 - chao.HeightRequest;
+		if (imgesquilo.TranslationY >= maxY)
+			return true;
+		else
+			return false;
+	}
+
+	async Task Desenha()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			GerenciaCanos();
+			if (VerificaColisaoChao())
+			{
+				estaMorto = true;
+				FrameGameOver.IsVisible = true;
+				break;
+			}
+
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+
+
+	bool VerificaColisao()
+	{
+		if (!estaMorto)
+		{
+			if (VerificaColisaoTeto() ||
+				VerificaColisaoChao())
+			{
+				return true;
+			}
+		}
+			return false;
+
+		
+	}
+
 }
+
+
 
